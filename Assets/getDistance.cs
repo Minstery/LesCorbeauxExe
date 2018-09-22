20c861faa24dd1f6bff8112e82493d4a141f9ec2
Unknown <oncheonche@ymail.com>
2018-09-22 13:23:32 +0200

    v3

---
+++ Assets/getDistance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getDistance : MonoBehaviour {
+
+  public Player Player;
+  Vector3 startPos;
+  public Text distance;
+
+  // Use this for initialization
+  void Start () {
+    startPos = Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    distance.text = "Distance : " + (int)Vector3.Distance(startPos, Player.transform.position) + "m";
+	}
+}
--- Assets/winOrLose.cs
+++ Assets/winOrLose.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class winOrLose : MonoBehaviour {
 
   public GameObject win;
+  public GameObject win2;
 
   void Update()
   {
     if (transform.position.x > 210)
     {
-      win.SetActive(true);
-      Destroy(this);
+      StartCoroutine("endCredits");
+      this.enabled = false;
     }
+  }
+
+  IEnumerator endCredits()
+  {
+    win.SetActive(true);
+    yield return new WaitUntil(() => Input.GetButtonDown("Jump"));
+    win.SetActive(false);
+    win2.SetActive(true);
+    yield return new WaitUntil(() => Input.GetButtonDown("Jump"));
+    Application.Quit();
+    Destroy(this);
   }
 }
